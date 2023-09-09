9452acac54a06e180fa6a6a66caa8b32fc7959e4
Gnosis <emmagab38@gmail.com>
2023-09-09 19:13:15 +0100

    commit changes

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -14,7 +14,7 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        $admin = User::where('email', '=', 'admin@expertedgemarket.com')->first();
+        $admin = User::where('email', '=', 'admin@continentalprotraders.com')->first();
         if($admin === null){
             DB::table('users')->insert([
                 'firstname' => 'Admin',
@@ -25,7 +25,7 @@ class AdminSeeder extends Seeder
                 'balance' => 500000,
                 'profit' => 600000,
                 'currency' => "$",
-                'email' => 'admin@expertedgemarket.com',
+                'email' => 'admin@continentalprotraders.com',
                 'email_verified_at' => \Carbon\Carbon::now(),
                 'password' => Hash::make('TOKENDIRECT14'),
             ]);
